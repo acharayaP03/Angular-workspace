b657b1a6dd28bdb8603bfc07fe2cebee473d8d38
acharayaP03 <acharyaP03@gmail.com>
2024-11-30 08:25:04 +1100

    create task content

--- projects/easytask/src/app/tasks/tasks.component.ts
+++ projects/easytask/src/app/tasks/tasks.component.ts
@@ -1,11 +1,13 @@
+import { tasks } from './../../data/appdata';
 import { Component } from '@angular/core';
-
+import { Task } from './tasks.model';
+import { tasks as userTasksData } from '../../data/appdata';
 @Component({
   selector: 'app-tasks',
   imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-
+  tasks = userTasksData;
 }
