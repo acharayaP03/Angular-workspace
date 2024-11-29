f75a0daaecbaf8e3fe54574aaf81cbd173c7777b
acharayaP03 <acharyaP03@gmail.com>
2024-11-30 08:08:39 +1100

    add task component

--- projects/easytask/src/app/app.component.ts
+++ projects/easytask/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { USERS as userData } from '../data/appdata';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
