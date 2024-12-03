44a7139cf50ed4ecb022d4a0982fe26fe2ecb223
acharayaP03 <acharyaP03@gmail.com>
2024-12-03 20:31:04 +1100

    component wrapper with props

--- projects/statusmanagement/src/app/app.component.ts
+++ projects/statusmanagement/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
-import { SupportTicketComponent } from "./dashboard/support-ticket/support-ticket.component";
-import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { SupportTicketComponent } from './dashboard/support-ticket/support-ticket.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, SupportTicketComponent, TrafficComponent],
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    SupportTicketComponent,
+    TrafficComponent,
+    DashboardItemComponent,
+  ],
 })
-export class AppComponent {
-
-  
-}
+export class AppComponent {}
