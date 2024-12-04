89c4caccda7eaeccbccf24096d1c190989dd0d9e
acharayaP03 <acharyaP03@gmail.com>
2024-12-04 11:00:56 +1100

    rendering component as default component rather than prefixed app compnent

--- projects/statusmanagement/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ projects/statusmanagement/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ControlComponent } from '../../../sharedUi/control/control.component';
+import { ButtonComponent } from '../../../sharedUi/button/button.component';
 @Component({
-  selector: 'app-new-ticket',
-  imports: [ControlComponent],
+  selector: 'from[appNewTicket]',
+  imports: [],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class NewTicketComponent {}
